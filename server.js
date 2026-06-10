17e25804d2a914a4071cf9cecdd675b95646e8d3
Phillip Neibaur <pneibaur@gmail.com>
2026-06-10 16:57:03 -0600

    adds data and links to server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT
+let transactions = require("./models/data.js")
+
+console.log(transactions)
 
 
 app.listen(port, ()=>{console.log("listening on port " + port)})
