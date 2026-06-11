9b0fd43b65de3da7aa6549cb5f1851940f854418
Phillip Neibaur <pneibaur@gmail.com>
2026-06-10 21:01:10 -0600

    adds show route and configures ejs page

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ require('dotenv').config()
 const port = process.env.PORT
 let transactions = require("./models/data.js")
 
-console.log(transactions)
+app.use(express.urlencoded({extended: false}))
 
+app.get("/transactions", (request, response)=>{
+    response.render("index.ejs", {transactions: transactions})
+})
+
+app.get("/transactions/:index", (request, response)=>{
+    response.render("show.ejs", {transaction: transactions[request.params.index]})
+})
 
 app.listen(port, ()=>{console.log("listening on port " + port)})
